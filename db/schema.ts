da817d55134b72be775c8db86e17009dfeb766c9
Matti Luukkainen <mluukkai@iki.fi>
2026-04-14 23:53:16 +0300

    p6

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,16 @@
-import { pgTable, serial, text, boolean } from "drizzle-orm/pg-core"
+import { pgTable, serial, text, boolean, integer } from "drizzle-orm/pg-core"
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  username: text("username").notNull().unique(),
+  name: text("name").notNull(),
+})
 
 export const notes = pgTable("notes", {
   id: serial("id").primaryKey(),
   content: text("content").notNull(),
   important: boolean("important").notNull().default(false),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id),
 })
